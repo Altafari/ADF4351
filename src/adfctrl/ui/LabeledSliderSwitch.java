442fdac006309b89ca14ca75239ad7c191c76f97
amh <amh@myhost.org>
2016-12-17 21:36:47 +0200

    Labeled switch is almost done

--- src/adfctrl/ui/LabeledSliderSwitch.java
+++ src/adfctrl/ui/LabeledSliderSwitch.java
@@ -1,5 +1,65 @@
 package adfctrl.ui;
 
-public class LabeledSliderSwitch {
+import java.awt.Color;
+import java.awt.Dimension;
+import java.util.Hashtable;
+import java.util.List;
 
+import javax.swing.BorderFactory;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JSlider;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+import adfctrl.utils.IObserver;
+import adfctrl.utils.Observable;
+
+public class LabeledSliderSwitch<T extends Enum<?>> extends JPanel implements IObserver<T>, ChangeListener{
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+    
+    private final int BORDER_PADDING = 5;
+    private final Observable<T> model;
+    private final List<T> states;
+    private final JSlider slider;
+    
+    public LabeledSliderSwitch(String title, Observable<T> model, List<T> states, List<String> labels) {
+        this.setBorder(BorderFactory.createCompoundBorder(
+                BorderFactory.createEmptyBorder(BORDER_PADDING, BORDER_PADDING,
+                        BORDER_PADDING, BORDER_PADDING),
+                        BorderFactory.createTitledBorder(
+                                BorderFactory.createLineBorder(Color.gray), title)));
+        
+        this.model = model;
+        this.states = states;
+        Hashtable<Object, Object> nLabels = new Hashtable<Object, Object>();
+        for (int i = 0; i < labels.size(); i++) {
+            nLabels.put(i, new JLabel(labels.get(i)));
+        }
+        slider = new JSlider(JSlider.VERTICAL, 0, states.size() - 1, 0);
+        slider.setMajorTickSpacing(1);
+        slider.setPaintLabels(true);
+        slider.setPaintTicks(true);
+        slider.setLabelTable(nLabels);
+        this.add(slider);
+        // TODO: set proper dimensions
+    }
+    
+    @Override
+    public void notifyChanged(T newVal) {
+        int idx = states.indexOf(newVal);
+        if (idx < 0) {
+            return;
+        }
+        slider.setValue(idx);
+    }
+
+    @Override
+    public void stateChanged(ChangeEvent e) {
+        model.setValue(states.get(slider.getValue()));
+    }
 }
--- src/adfctrl/ui/MainForm.java
+++ src/adfctrl/ui/MainForm.java
@@ -2,10 +2,15 @@ package adfctrl.ui;
 
 import java.awt.BorderLayout;
 import java.awt.Container;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
+import adfctrl.icmodel.ADF4351Configurator;
+import adfctrl.icmodel.ADF4351Configurator.SynthMode;
 import adfctrl.system.SystemManager;
 import adfctrl.utils.Observable;
 
@@ -22,9 +27,20 @@ public class MainForm {
     
     private static void addComponentsToPane(Container pane) {
         JPanel ctrlPanel = new JPanel();
-        LabeledNumericField integerValueField = 
-                new LabeledNumericField("Test", 6, (v) -> v > 0 && v < 100); // TODO: move validator to model  
+        SystemManager sysMgr = SystemManager.getInstance();
+        ADF4351Configurator config = sysMgr.getConfigurator();
+        
+        LabeledNumericField integerValueField = new LabeledNumericField(
+                "Test", 6, (v) -> v > 0 && v < 100); // TODO: move validator to model         
         ctrlPanel.add(integerValueField);
+        
+        
+        List<SynthMode> stateList = Arrays.asList(SynthMode.INTEGER, SynthMode.FRACTIONAL);
+        List<String> labels = Arrays.asList("Integer", "Fractional");
+        LabeledSliderSwitch<SynthMode> synthModeSwitch = new LabeledSliderSwitch<SynthMode>(
+                "Mode", config.synthMode, stateList, labels);
+        ctrlPanel.add(synthModeSwitch);
+        
         Observable<Integer> intVal = SystemManager.getInstance().getConfigurator().intValue; 
         integerValueField.setModel(intVal);
         pane.add(ctrlPanel, BorderLayout.LINE_START); 
